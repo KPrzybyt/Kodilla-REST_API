db74788ecaddab3e149f90a7f47af816ce4e6e96
krzysztofprzybyt <krzysztof.przybyt@gmail.com>
2018-02-24 23:36:46 +0100

    17.4

--- showtasks.sh
+++ showtasks.sh
@@ -1,3 +1,26 @@
 #!/usr/bin/env bash
 
 
+fail() {
+  echo "There were errors"
+}
+
+end() {
+  echo "Work is finished"
+}
+setFirefox() {
+    xdg-settings set default-web-browser firefox.desktop
+}
+runTasksInBrowser() {
+
+  xdg-open http://localhost:8080/crud/v1/task/getTasks
+}
+
+if ./runcrud.sh; then
+   setFirefox
+   runTasksInBrowser
+
+else
+
+   fail
+fi
